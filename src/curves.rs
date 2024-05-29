cf03f5fdb19f56d2b7fefa98539a9991cec05588
Tristram Gräbener <tristram+git@tristramg.eu>
2024-05-29 11:40:47 +0200

    Curve: use geodesic lengths when relevant

--- src/curves.rs
+++ src/curves.rs
@@ -270,8 +270,9 @@ impl SphericalLineStringCurve {
     /// Splits the [`LineString`] into smaller [`Curve`]s of at most `max_len` length.
     /// If the initial geometry is invalid, it returns an empty vector.
     pub fn new_fragmented(geom: LineString, max_len: f64, max_extent: f64) -> Vec<Self> {
-        let n = (geom.haversine_length() / max_len).ceil() as usize;
+        let n = (geom.geodesic_length() / max_len).ceil() as usize;
 
+        // There is no geodesic segmentize, but this is not a problem as exact length of each segment isn’t relevant
         geom.line_segmentize_haversine(n)
             .map(|multi| {
                 multi
@@ -286,7 +287,7 @@ impl SphericalLineStringCurve {
 
 impl Curve for SphericalLineStringCurve {
     fn new(geom: LineString, max_extent: f64) -> Self {
-        let length = geom.haversine_length();
+        let length = geom.geodesic_length();
         Self {
             start_offset: 0.,
             max_extent,
@@ -358,15 +359,15 @@ impl Curve for SphericalLineStringCurve {
 
         // go through each segment and look for the one that frame the distance that we seek
         for segment in self.geom.lines() {
-            let segment_length = segment.haversine_length();
+            let segment_length = segment.geodesic_length();
             if accumulated_length + segment_length >= fractional_length {
                 let segment_fraction = (fractional_length - accumulated_length) / segment_length;
                 let segment_start = Point::from(segment.start);
                 let segment_end = Point::from(segment.end);
 
-                // get the Point at a haversine distance between two Points
+                // get the Point at a geodesic distance between two Points
                 // of a certain fraction of length on this distance
-                return Ok(segment_start.haversine_intermediate(&segment_end, segment_fraction));
+                return Ok(segment_start.geodesic_intermediate(&segment_end, segment_fraction));
             }
             accumulated_length += segment_length;
         }
@@ -380,13 +381,13 @@ impl Curve for SphericalLineStringCurve {
 
         // add max_extend distance in South and then West direction
         let min_point_extended = min_point
-            .haversine_destination(180., self.max_extent)
-            .haversine_destination(270., self.max_extent);
+            .geodesic_destination(180., self.max_extent)
+            .geodesic_destination(270., self.max_extent);
 
         // add max_extend distance in North and then East direction
         let max_point_extended = max_point
-            .haversine_destination(0., self.max_extent)
-            .haversine_destination(90., self.max_extent);
+            .geodesic_destination(0., self.max_extent)
+            .geodesic_destination(90., self.max_extent);
 
         Rect::new(min_point_extended, max_point_extended)
     }
@@ -420,17 +421,17 @@ impl Curve for SphericalLineStringCurve {
         // go through each segment and look for the one that frame the distance that we seek
         let mut accumulated_length = 0.;
         for segment in self.geom.densify_haversine(self.densify_by).lines() {
-            let segment_length = segment.haversine_length();
+            let segment_length = segment.geodesic_length();
             if accumulated_length + segment_length >= offset {
                 // get Points from the segment that frame the point
                 let start = geo::Point(segment.start);
                 let end = geo::Point(segment.end);
 
                 // get bearing from start Point and end Point of the segment, and add 90° clockwise rotation to it
-                let normal_vector_bearing = start.haversine_bearing(end) + 90.;
+                let normal_vector_bearing = start.geodesic_bearing(end) + 90.;
 
                 // get end Point from the end of the segment for the normal vector bearing value and 1m of (haversine) length
-                let end_normal = end.haversine_destination(normal_vector_bearing, 1.);
+                let end_normal = end.geodesic_destination(normal_vector_bearing, 1.);
 
                 return Ok((end_normal.x() - end.x(), end_normal.y() - end.y()));
             }
@@ -450,7 +451,7 @@ pub struct CurveProjection {
 
     /// How far is the [`Point`] from the [`Curve`]:
     /// - [`geo::algorithm::euclidean_distance`] for [`PlanarLineStringCurve`]
-    /// - [`geo::algorithm::haversine_distance`] for [`SphericalLineStringCurve`]
+    /// - [`geo::algorithm::geodesic_distance`] for [`SphericalLineStringCurve`]
     ///
     /// The distance is `positive` if the [`Point`] is located on the left of the [`Curve`]
     /// and `negative` if the [`Point`] is on the right.
@@ -589,25 +590,24 @@ mod tests {
             1.,
         );
 
-        assert_eq!(5837416, paris_to_new_york.len());
+        assert_eq!(5853102, paris_to_new_york.len());
         assert_relative_eq!(
             framentation_max_length,
             paris_to_new_york[0].length(),
-            epsilon = 1e-6
+            epsilon = 1e-4
         );
-        // 1e-6 means we lose 1. micrometer per segment
     }
 
     #[test]
     fn spherical_length() {
         let paris_to_new_york = SphericalLineStringCurve::new(line_string![PARIS, NEW_YORK], 1.);
-        assert_eq!(5837415.205720471, paris_to_new_york.length()); // 5837415.205720471 using [`HaversineLength`] and 5853101.331803938 using [`GeodesicLength`]
+        assert_eq!(5853101.331803938, paris_to_new_york.length()); // 5837415.205720471 using [`HaversineLength`] and 5853101.331803938 using [`GeodesicLength`]
 
         let lille_to_perpignan = SphericalLineStringCurve::new(line_string![LILLE, PERPIGNAN], 1.);
-        assert_eq!(882995.0489150163, lille_to_perpignan.length()); // 882995.0489150163 using [`HaversineLength`] and 882749.856002331 using [`GeodesicLength`]
+        assert_eq!(882749.856002331, lille_to_perpignan.length()); // 882995.0489150163 using [`HaversineLength`] and 882749.856002331 using [`GeodesicLength`]
 
         let brest_to_nancy = SphericalLineStringCurve::new(line_string![BREST, NANCY], 1.);
-        assert_eq!(785611.8752324395, brest_to_nancy.length()); // 785611.8752324395 using [`HaversineLength`] and 787969.3534391255 using [`GeodesicLength`]
+        assert_eq!(787969.3534391255, brest_to_nancy.length()); // 785611.8752324395 using [`HaversineLength`] and 787969.3534391255 using [`GeodesicLength`]
     }
 
     #[test]
@@ -654,23 +654,23 @@ mod tests {
         let projected = paris_to_new_york
             .project(point! {x: -6.71, y: 51.42})
             .unwrap();
-        assert_eq!(701868.9818283478, projected.distance_along_curve);
-        assert_eq!(-66665.6982638038, projected.offset);
+        assert_eq!(665932.1048021464, projected.distance_along_curve);
+        assert_eq!(-388790.4195662504, projected.offset);
 
         // Point is located on the left (south) of the curve
         let projected = paris_to_new_york
             .project(point! {x: -12.25, y: 45.86})
             .unwrap();
-        assert_eq!(963056.4000812048, projected.distance_along_curve);
-        assert_eq!(625574.8001320735, projected.offset);
+        assert_eq!(1130772.559389318, projected.distance_along_curve);
+        assert_eq!(158889.02911883662, projected.offset);
 
         // Same point, but with an offset from the curve
         paris_to_new_york.start_offset = 1000000.;
         let projected = paris_to_new_york
             .project(point! {x: -12.25, y: 45.86})
             .unwrap();
-        assert_eq!(1963056.4000812047, projected.distance_along_curve);
-        assert_eq!(625574.8001320735, projected.offset);
+        assert_eq!(2130772.5593893183, projected.distance_along_curve);
+        assert_eq!(158889.02911883662, projected.offset);
 
         let mut new_york_to_paris =
             SphericalLineStringCurve::new(line_string![NEW_YORK, PARIS], 1.);
@@ -679,23 +679,23 @@ mod tests {
         let projected = new_york_to_paris
             .project(point! {x: -6.71, y: 51.42})
             .unwrap();
-        assert_eq!(5135546.223892125, projected.distance_along_curve);
-        assert_eq!(66665.6982638038, projected.offset);
+        assert_eq!(5187169.227001794, projected.distance_along_curve);
+        assert_eq!(388790.4195662507, projected.offset);
 
         // Point is located on the right (south)  of the curve
         let projected = new_york_to_paris
             .project(point! {x: -12.25, y: 45.86})
             .unwrap();
-        assert_eq!(4874358.805639268, projected.distance_along_curve);
-        assert_eq!(-625574.8001320735, projected.offset);
+        assert_eq!(4722328.772414621, projected.distance_along_curve);
+        assert_eq!(-158889.02911883662, projected.offset);
 
         // Same point, but with an offset from the curve
         new_york_to_paris.start_offset = 1000000.;
         let projected = new_york_to_paris
             .project(point! {x: -12.25, y: 45.86})
             .unwrap();
-        assert_eq!(5874358.805639268, projected.distance_along_curve);
-        assert_eq!(-625574.8001320735, projected.offset);
+        assert_eq!(5722328.772414621, projected.distance_along_curve);
+        assert_eq!(-158889.02911883662, projected.offset);
     }
 
     #[test]
@@ -708,13 +708,13 @@ mod tests {
             offset: 0.,
         };
         let paris_to_new_york_projection = paris_to_new_york.resolve(projection).unwrap();
-        assert_eq!(-11.117252327440333, paris_to_new_york_projection.x());
-        assert_eq!(51.44598457247205, paris_to_new_york_projection.y());
+        assert_eq!(-11.073026969801687, paris_to_new_york_projection.x());
+        assert_eq!(51.452453982109404, paris_to_new_york_projection.y());
 
         paris_to_new_york.start_offset = 300000.;
         let paris_to_new_york_projection = paris_to_new_york.resolve(projection).unwrap();
-        assert_eq!(-6.927740203201038, paris_to_new_york_projection.x());
-        assert_eq!(50.835999775637625, paris_to_new_york_projection.y());
+        assert_eq!(-6.8972570655588346, paris_to_new_york_projection.x());
+        assert_eq!(50.83999834131466, paris_to_new_york_projection.y());
 
         projection.distance_along_curve = 8000000.;
         assert!(paris_to_new_york.resolve(projection).is_err());
@@ -727,8 +727,8 @@ mod tests {
             offset: 0.,
         };
         let lille_to_perpignan_p = lille_to_perpignan.resolve(projection).unwrap();
-        assert_eq!(2.961652543888961, lille_to_perpignan_p.x());
-        assert_eq!(46.13397259680868, lille_to_perpignan_p.y());
+        assert_eq!(2.961644856565597, lille_to_perpignan_p.x());
+        assert_eq!(46.13408148827718, lille_to_perpignan_p.y());
 
         // Test on a linestring where only the latitude changes (longitude remains almost the same)
         let brest_to_nancy = SphericalLineStringCurve::new(line_string![BREST, NANCY], 1.);
@@ -738,8 +738,8 @@ mod tests {
             offset: 0.,
         };
         let brest_to_nancy_p = brest_to_nancy.resolve(projection).unwrap();
-        assert_eq!(2.288400083145514, brest_to_nancy_p.x());
-        assert_eq!(48.69523589360801, brest_to_nancy_p.y());
+        assert_eq!(2.268067652986713, brest_to_nancy_p.x());
+        assert_eq!(48.695256847531994, brest_to_nancy_p.y());
     }
 
     #[test]
@@ -758,11 +758,11 @@ mod tests {
 
         assert_eq!(
             bbox.min(),
-            coord! {x: -7.651445898208635, y: 51.53910067075082}
+            coord! { x: -7.651441315145896, y: 51.539101184127524 }
         );
         assert_eq!(
             bbox.max(),
-            coord! {x: -6.388541186830253, y: 51.9408993113492}
+            coord! { x: -6.388545844195035, y: 51.940898736377044 }
         );
     }
 
@@ -819,7 +819,7 @@ mod tests {
         assert_relative_eq!(
             latitudinal_normal.0,
             normalized_translation_on_earth,
-            epsilon = 1e-8
+            epsilon = 1e-5
         );
         assert_relative_eq!(latitudinal_normal.1, 0., epsilon = 1e-7);
     }
